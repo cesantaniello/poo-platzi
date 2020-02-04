6ede5742c119b1764400559d7672b1fe9614c51a
Carlos <carlos.e.santaniello@gmail.com>
2020-02-04 21:52:49 +0100

    Encapsulamiento en Java

--- Java/Car.java
+++ Java/Car.java
@@ -2,7 +2,7 @@ class Car {
   Integer id;
   String license;
   Account driver;
-  Integer passenger;
+  private Integer passenger;
 
   public Car(String license, Account driver) {
     this.license = license;
@@ -10,6 +10,20 @@ class Car {
   }
 
   void printDataCar() {
-    System.out.println("License: " + license + " Name Driver: " + driver.name);
+    if(passenger != null){
+      System.out.println("License: " + license + " Name Driver: " + driver.name + " Passengers: " + passenger);
+    }
+  }
+
+  public Integer getPassenger(){
+    return passenger;
+  }
+
+  public void setPassenger(Integer passenger){
+    if(passenger == 4){
+      this.passenger = passenger;
+    } else {
+      System.out.println("Necesitas asignar 4 pasajeros");
+    }  
   }
 }
