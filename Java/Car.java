da766929ce0e0b4784b6d3601e37f66c7e68bf6d
Carlos <carlos.e.santaniello@gmail.com>
2020-02-05 17:31:09 +0100

    Agregado polimorfismo en Java

--- Java/Car.java
+++ Java/Car.java
@@ -1,7 +1,7 @@
 class Car {
-  Integer id;
-  String license;
-  Account driver;
+  private Integer id;
+  private String license;
+  private Account driver;
   private Integer passenger;
 
   public Car(String license, Account driver) {
@@ -26,4 +26,47 @@ class Car {
       System.out.println("Necesitas asignar 4 pasajeros");
     }  
   }
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+      return id;
+  }
+
+  /**
+   * @param id the id to set
+   */
+  public void setId(Integer id) {
+      this.id = id;
+  }
+
+  /**
+   * @return the license
+   */
+  public String getLicense() {
+      return license;
+  }
+
+  /**
+   * @param license the license to set
+   */
+  public void setLicense(String license) {
+      this.license = license;
+  }
+
+  /**
+   * @return the driver
+   */
+  public Account getDriver() {
+      return driver;
+  }
+
+  /**
+   * @param driver the driver to set
+   */
+  public void setDriver(Account driver) {
+      this.driver = driver;
+  }
+
 }
